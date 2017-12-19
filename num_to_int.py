b8fdb51037557b303b768d50ed1ce26c602e9b8b
VincentGitHub15 <vjchiodo@verizon.net>
2017-12-18 23:07:53 -0500

    number to integer

---
+++ num_to_int.py
@@ -0,0 +1,51 @@
+
+ones_dict = { 
+			'one':   1, 'eleven':     11,
+			'two':   2, 'twelve':     12,
+			'three': 3, 'thirteen':   13,
+			'four':  4, 'fourteen':   14,
+			'five':  5, 'fifteen':    15,
+			'six':   6, 'sixteen':    16,
+			'seven': 7, 'seventeen':  17,
+			'eight': 8, 'eighteen':   18,
+			'nine':  9, 'nineteen':   19 
+			}
+
+tens_dict = {
+	'ten':	10,
+	'twenty': 20,
+	'thirty': 30,
+	'forty': 40,
+	'fifty': 50,
+	'sixty': 60,
+	'seventy': 70,
+	'eighty': 80,
+	'ninety': 90,
+
+}
+
+groups_dict = {
+	'hundred': 100,
+	'thousand': 1000,
+	'million': 100000000
+}
+
+# print(ones_dict["one"])
+
+number_example = ["three hundred ninety nine million four hundred twenty one thousand seven hundred fourteen"];
+
+# if number_example in tens_dict:
+# 	print(number_example)
+
+# print (len number_example[0[1]])
+
+for i in number_example:
+	print(i)
+	if i == ' ':
+		print("there is a space")
+	# print(i)
+
+
+
+
+
