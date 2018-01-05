c678dd0ed3d194d04ed5757080c83570031e67cd
VincentGitHub15 <vjchiodo@verizon.net>
2018-01-05 01:10:48 -0500

    use string instead of tuple

--- ints_to_sum.py
+++ ints_to_sum.py
@@ -1,8 +1,16 @@
 #print all integer combos that add to final integer in order
 
 
-vals = 1,3,5,8,9;52
+vals = '1,3,5,8,9;52'
 
-for i in vals:
-	print(i)
+for i in range(len(vals)):
+	print(vals[i])
 
+# print(vals)
+
+# vals = str(vals)
+# print(vals)
+
+# print(type(vals))
+# print(vals[-2])
+# print(vals)
